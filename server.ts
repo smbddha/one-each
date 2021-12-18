d302af76acc78642e2c94f2018e2b806d12011cd
smbddha <samuelwlaing@gmail.com>
2021-12-18 15:19:47 -0500

    small changes; dotenv, mysql added

--- server.ts
+++ server.ts
@@ -1,6 +1,20 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import * as Eta from 'eta';
+import mysql from 'mysql';
 import requestIp from 'request-ip';
+
+dotenv.config();
+
+// process.env.DB_USER
+// process.env.DB_KEY
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+})
 
 Eta.configure({
     plugins: [],
@@ -17,8 +31,11 @@ app.set("views", "./templates");
 
 app.use(requestIp.mw());
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
     const msg = "WHAT";
+
+    // TODO validate this
+    const cip = req.clientIp;
 
     res.render("index", {
         message: msg
